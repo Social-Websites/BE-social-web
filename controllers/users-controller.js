49397c4853a2997fab93fe0e54a8e930dc1ee42c
gitdanh <thanhdanhnguyen46@gmail.com>
2023-11-29 09:21:43 +0700

    friend requests

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
@@ -131,7 +132,7 @@ const getFriendRequestsList = async (req, res, next) => {
   try {
     // Tìm user theo userId và populate friend_requests
     const user = await User.findById(userId).populate({
-      path: "friends",
+      path: "friend_requests",
       select: "username full_name profile_picture",
       options: {
         limit: limit,
@@ -155,6 +156,168 @@ const getFriendRequestsList = async (req, res, next) => {
   }
 };
 
+const sendAddFriendRequest = async (req, res, next) => {
+  const userId = req.userData.id;
+  const userIdToSend = req.params.userId;
+
+  try {
+    const [user, userToSend] = await Promise.all([
+      User.findById(userId).select("friend_requests_sent"),
+      User.findById(userIdToSend).select("friend_requests"),
+    ]);
+    if (!user) {
+      const error = new HttpError("Không tìm thấy user!", 404);
+      return next(error);
+    }
+    if (!userToSend) {
+      const error = new HttpError("Không tìm thấy user cần kết bạn!", 404);
+      return next(error);
+    }
+
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    user.friend_requests_sent.push(userToSend._id);
+    await user.save({ session: sess });
+    userToSend.friend_requests.push(user._id);
+    await userToSend.save({ session: sess });
+
+    await sess.commitTransaction();
+  } catch (err) {
+    console.log("Add Friend 1===============: ", err);
+    const error = new HttpError(
+      "Có lỗi khi gửi lời mời, vui lòng thử lại!",
+      500
+    );
+    return next(error);
+  }
+
+  res.json({ message: "Đã gửi lời mời!" });
+};
+
+const acceptAddFriendRequest = async (req, res, next) => {
+  const userId = req.userData.id;
+  const userIdToAdd = req.params.userId;
+
+  try {
+    const [user, userToAdd] = await Promise.all([
+      User.findById(userId).select("friends friend_requests"),
+      User.findById(userIdToAdd).select("friends friend_requests_sent"),
+    ]);
+    if (!user) {
+      const error = new HttpError("Không tìm thấy user!", 404);
+      return next(error);
+    }
+    if (!userToAdd) {
+      const error = new HttpError("Không tìm thấy user để chấp nhận!", 404);
+      return next(error);
+    }
+
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+
+    // Thêm vào mảng friends của user và pull từ friend_requests_sent của user
+    user.friend_requests.pull(userToAdd._id);
+    user.friends.push(userToAdd._id);
+
+    // Pull từ friend_requests của userToAdd
+    userToAdd.friend_requests_sent.pull(user._id);
+    userToAdd.friends.push(user._id);
+
+    await user.save({ session: sess });
+    await userToAdd.save({ session: sess });
+
+    await sess.commitTransaction();
+  } catch (err) {
+    console.log("Accept Friend Request==============: ", err);
+    const error = new HttpError("Có lỗi khi chấp nhận, vui lòng thử lại!", 500);
+    return next(error);
+  }
+
+  res.json({ message: "Đã chấp nhận lời mời kết bạn!" });
+};
+
+const removeAddFriendRequest = async (req, res, next) => {
+  const userId = req.userData.id;
+  const userIdToRemove = req.params.userId;
+
+  try {
+    const [user, userToRemove] = await Promise.all([
+      User.findById(userId).select("friend_requests_sent"),
+      User.findById(userIdToRemove).select("friend_requests"),
+    ]);
+    if (!user) {
+      const error = new HttpError("Không tìm thấy user!", 404);
+      return next(error);
+    }
+    if (!userToRemove) {
+      const error = new HttpError(
+        "Không tìm thấy user cần rút lại lời mời!",
+        404
+      );
+      return next(error);
+    }
+
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+
+    // Pull từ cả hai mảng friend_requests_sent và friend_requests
+    user.friend_requests_sent.pull(userToRemove._id);
+    userToRemove.friend_requests.pull(user._id);
+
+    await user.save({ session: sess });
+    await userToRemove.save({ session: sess });
+
+    await sess.commitTransaction();
+  } catch (err) {
+    console.log("Reject Friend Request==============: ", err);
+    const error = new HttpError(
+      "Có lỗi khi rút lời mời, vui lòng thử lại!",
+      500
+    );
+    return next(error);
+  }
+
+  res.json({ message: "Đã rút lại lời mời kết bạn!" });
+};
+
+const rejectAddFriendRequest = async (req, res, next) => {
+  const userId = req.userData.id;
+  const userIdToReject = req.params.userId;
+
+  try {
+    const [user, userToReject] = await Promise.all([
+      User.findById(userId).select("friend_requests"),
+      User.findById(userIdToReject).select("friend_requests_sent"),
+    ]);
+    if (!user) {
+      const error = new HttpError("Không tìm thấy user!", 404);
+      return next(error);
+    }
+    if (!userToReject) {
+      const error = new HttpError("Không tìm thấy user cần từ chối!", 404);
+      return next(error);
+    }
+
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+
+    // Pull từ cả hai mảng friend_requests_sent và friend_requests
+    user.friend_requests.pull(userToReject._id);
+    userToReject.friend_requests_sent.pull(user._id);
+
+    await user.save({ session: sess });
+    await userToReject.save({ session: sess });
+
+    await sess.commitTransaction();
+  } catch (err) {
+    console.log("Reject Friend Request==============: ", err);
+    const error = new HttpError("Có lỗi khi từ chối, vui lòng thử lại!", 500);
+    return next(error);
+  }
+
+  res.json({ message: "Đã từ chối lời mời kết bạn!" });
+};
+
 const searchUsers = async (req, res, next) => {
   const searchText = req.query.searchText;
   console.log(searchText);
@@ -176,8 +339,13 @@ const searchUsers = async (req, res, next) => {
     return next(error + searchText);
   }
 };
+
 exports.searchUsers = searchUsers;
 exports.getUser = getUser;
 exports.getUserByUsername = getUserByUsername;
 exports.getUserFriendsListByUsername = getUserFriendsListByUsername;
 exports.getFriendRequestsList = getFriendRequestsList;
+exports.sendAddFriendRequest = sendAddFriendRequest;
+exports.acceptAddFriendRequest = acceptAddFriendRequest;
+exports.removeAddFriendRequest = removeAddFriendRequest;
+exports.rejectAddFriendRequest = rejectAddFriendRequest;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -14,5 +14,21 @@ router.get("/auth-user", UsersController.getUser);
 router.get("/:username", UsersController.getUserByUsername);
 router.get("/:username/friends", UsersController.getUserFriendsListByUsername);
 router.get("/auth-user/friend-requests", UsersController.getFriendRequestsList);
+router.patch(
+  "/friend-requests/send/:userId",
+  UsersController.sendAddFriendRequest
+);
+router.patch(
+  "/friend-requests/accept/:userId",
+  UsersController.acceptAddFriendRequest
+);
+router.patch(
+  "/friend-requests/remove/:userId",
+  UsersController.removeAddFriendRequest
+);
+router.patch(
+  "/friend-requests/reject/:userId",
+  UsersController.rejectAddFriendRequest
+);
 
 module.exports = router;
