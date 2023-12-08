66d71d929d5e4ac9172daed25594633462ccaea9
gitdanh <thanhdanhnguyen46@gmail.com>
2023-12-08 17:30:31 +0700

    un install bcrypt

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const HttpError = require("./http-error");
 
 const { Schema, Types } = mongoose;
